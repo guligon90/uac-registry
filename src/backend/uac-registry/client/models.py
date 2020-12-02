fe9df1623509dadfd462f9bf37048986fc03b2e4
Guilherme Lima Gonçalves <guilherme.lima@ahgora.com.br>
2020-12-02 00:29:48 -0300

    feat: implement client DB model

--- src/backend/uac-registry/client/models.py
+++ src/backend/uac-registry/client/models.py
@@ -1,3 +1,14 @@
+# Django imports
 from django.db import models
+from django_extensions.db.models import TimeStampedModel
 
-# Create your models here.
+
+class Client(TimeStampedModel):
+    """A client data abstraction"""
+    email = models.EmailField(max_length=40, unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
